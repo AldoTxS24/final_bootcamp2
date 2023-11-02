103f8064071fe6ea0e58f0c661b562f1537f84d9
Aldo Texis <44851792+AldoTxS24@users.noreply.github.com>
2023-11-01 19:11:52 -0600

    Update fabfile.py

--- movies/fabfile.py
+++ movies/fabfile.py
@@ -1,11 +1,20 @@
 from fabric.api import task
 from fabric.api import local
 from fabric.api import run #ejecuta un comando en un servidor remoto
+from fabric.api import env
+from fabric.api import prefix
+from fabric.api import sudo
+from fabric.api import cd
 
-@task(alias='ls')
-def show_dirs():
-    print("Hola world")
-    run('ls')
+env.hosts = ['165.232.140.48']
+env.user = 'aldo'
 
-def run_deploy_if_test_completed():
-    pass
+def deploy():
+    with cd('/home/aldo/project/final_bootcamp2/movies'):
+        run('git pull')
+        with prefix('source env/bin/activate'):
+            run('pip install -r requirements2.txt')
+            run('python manage.py migrate')
+
+            sudo('systemctl restart movies')
+            sudo('systemctl restart nginx')
